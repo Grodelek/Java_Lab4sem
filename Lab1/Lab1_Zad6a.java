701f6deeea3b9af5dfad3b2c1ae9fb3724fa74fe
A.Grodel <98063447+Grodelek@users.noreply.github.com>
2024-03-06 18:40:57 +0100

    Update Lab1_Zad6a.java

--- Lab1/Lab1_Zad6a.java
+++ Lab1/Lab1_Zad6a.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("*-----------*");
-        System.out.println("Pole trojkata");
         System.out.println();
         System.out.println("Podaj bok a:");
         double a = sc.nextDouble();
@@ -11,13 +11,11 @@ public class Main {
         double b = sc.nextDouble();
         System.out.println("Podaj bok c:");
         double c = sc.nextDouble();
-        System.out.println("Podaj wysokosc:");
-        double h = sc.nextDouble();
-        Trojkat jakis3kat = new Trojkat(a,h);
-        Trojkat jakis3kat2 = new Trojkat(a, b, c);
+        Trojkat jakis3kat = new Trojkat(a, b, c);
         double pole = jakis3kat.pole();
-        double obwod = jakis3kat2.obwod();
-        System.out.println("Pole trojkata: "+pole);
+        double obwod = jakis3kat.obwod();
+
+        System.out.println("Pole trojkata (wg wzoru Herona): "+pole);
         System.out.println("Obwod trojkata: "+obwod);
 
     }
@@ -26,18 +24,14 @@ class Trojkat{
     double a;
     double b;
     double c;
-    double h;
-    Trojkat(double a, double h){
-    this.a = a;
-    this.h = h;
-    }
+
     Trojkat(double a,double b,double c){
         this.a = a;
         this.b = b;
         this.c = c;
     }
     double pole(){
-        return (a*h)/2;
+        return (a+b+c)/2;
     }
     double obwod(){
         return a+b+c;
