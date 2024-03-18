0189f37266daec1bd908b7c0f62ad6a348e9b652
A.Grodel <98063447+Grodelek@users.noreply.github.com>
2024-03-18 18:27:28 +0100

    Update Lab3_Zad4.java

--- Lab3/Lab3_Zad4.java
+++ Lab3/Lab3_Zad4.java
@@ -6,19 +6,21 @@ public class Main {
     int[] tab = new int[5];
     int porownanie = 0;
     int skroc = 1;
+    int ostZam = tab.length - 1;
         for(int i=0; i<tab.length; i++){
                 System.out.println("Podaj tab["+i+"]");
                 tab[i] = sc.nextInt();
             }
         for(int i=0; i < tab.length-1; i++){
             skroc=1;
-            for(int j=0; j <tab.length - i - skroc; j++){
+            for(int j=0; j < ostZam; j++){
                 porownanie++;
                 if(tab[j] > tab[j+1]){
                     int temp = tab[j];
                     tab[j] = tab[j+1];
                     tab[j+1] = temp;
                     skroc++;
+                    ostZam = j;
                 }
             }
         }
