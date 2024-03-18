c462a68c6f20b3c2faa0340541dfc5ff7e17c379
A.Grodel <98063447+Grodelek@users.noreply.github.com>
2024-03-18 18:14:23 +0100

    Create Lab3_Zad3.java

---
+++ Lab3_Zad3.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int[] tab = new int[5];
+    int porownanie = 0;
+    int skroc = 1;
+        for(int i=0; i<tab.length; i++){
+                System.out.println("Podaj tab["+i+"]");
+                tab[i] = sc.nextInt();
+            }
+        for(int i=0; i < tab.length-1; i++){
+            skroc=1;
+            for(int j=0; j <tab.length - i - skroc; j++){
+                porownanie++;
+                if(tab[j] > tab[j+1]){
+                    int temp = tab[j];
+                    tab[j] = tab[j+1];
+                    tab[j+1] = temp;
+                    skroc++;
+                }
+            }
+        }
+        System.out.println("Liczba porownan: "+porownanie);
+       for(int element: tab){
+           System.out.println(element);
+       }
+    }
+}
